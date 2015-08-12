b92faeafe4eb93f524131348db8b8d4d2ba8947a
acanimal <asantiago@acuriousanimal.com>
2015-08-12 18:44:29 +0200

    Improved tests

--- test/query.test.js
+++ test/query.test.js
@@ -193,43 +193,320 @@ describe("cache", function() {
   });
 
 
-  it("should allow pass the request because has some parameter", function(done) {
-    var options = {
-      port: 8888,
-      logfile: path.join(__dirname, "..", "tmp", "clyde.log"),
-      loglevel: "info",
+  describe("allow", function() {
 
-      prefilters: [
-        {
-          id: "query-filter",
-          path: path.join(__dirname, "../lib/index.js"),
-          config: {
-            allow: {
-              any: ["a", "b", "c"]
+    it("should allow pass the request because has some 'any' parameters", function(done) {
+      var options = {
+        port: 8888,
+        logfile: path.join(__dirname, "..", "tmp", "clyde.log"),
+        loglevel: "info",
+
+        prefilters: [
+          {
+            id: "query-filter",
+            path: path.join(__dirname, "../lib/index.js"),
+            config: {
+              allow: {
+                any: ["a", "b", "c"]
+              }
             }
           }
-        }
-      ],
+        ],
 
-      providers: [
-        {
-          id: "id",
-          context: "/provider",
-          target: "http://localhost:8888"
-        }
-      ]
-    };
+        providers: [
+          {
+            id: "id",
+            context: "/provider",
+            target: "http://localhost:8888"
+          }
+        ]
+      };
 
-    // Create server with clyde's middleware options
-    var middleware = clyde.createMiddleware(options);
-    server = http.createServer(middleware);
-    server.listen(options.port);
+      // Create server with clyde's middleware options
+      var middleware = clyde.createMiddleware(options);
+      server = http.createServer(middleware);
+      server.listen(options.port);
 
-    // Make request which expects a 404 error
-    request("http://localhost:8888")
-      .get("/foo?a")
-      .expect(40, done);
+      // Make request which expects a 404 error
+      request("http://localhost:8888")
+        .get("/foo?c")
+        .expect(404, done);
+    });
+
+
+    it("should fail because the request does not have any of the required 'any' parameters", function(done) {
+      var options = {
+        port: 8888,
+        logfile: path.join(__dirname, "..", "tmp", "clyde.log"),
+        loglevel: "info",
+
+        prefilters: [
+          {
+            id: "query-filter",
+            path: path.join(__dirname, "../lib/index.js"),
+            config: {
+              allow: {
+                any: ["a", "b"]
+              }
+            }
+          }
+        ],
+
+        providers: [
+          {
+            id: "id",
+            context: "/provider",
+            target: "http://localhost:8888"
+          }
+        ]
+      };
+
+      // Create server with clyde's middleware options
+      var middleware = clyde.createMiddleware(options);
+      server = http.createServer(middleware);
+      server.listen(options.port);
+
+      // Make request which expects a 404 error
+      request("http://localhost:8888")
+        .get("/foo?c&d")
+        .expect(400, done);
+    });
+
+    it("should allow pass the request because has all the 'all' parameters", function(done) {
+      var options = {
+        port: 8888,
+        logfile: path.join(__dirname, "..", "tmp", "clyde.log"),
+        loglevel: "info",
+
+        prefilters: [
+          {
+            id: "query-filter",
+            path: path.join(__dirname, "../lib/index.js"),
+            config: {
+              allow: {
+                all: ["a", "b", "c"]
+              }
+            }
+          }
+        ],
+
+        providers: [
+          {
+            id: "id",
+            context: "/provider",
+            target: "http://localhost:8888"
+          }
+        ]
+      };
+
+      // Create server with clyde's middleware options
+      var middleware = clyde.createMiddleware(options);
+      server = http.createServer(middleware);
+      server.listen(options.port);
+
+      // Make request which expects a 404 error
+      request("http://localhost:8888")
+        .get("/foo?a&b=10&c&d=hi")
+        .expect(404, done);
+    });
+
+
+    it("should fail because the request does not have all the required 'all' parameters", function(done) {
+      var options = {
+        port: 8888,
+        logfile: path.join(__dirname, "..", "tmp", "clyde.log"),
+        loglevel: "info",
+
+        prefilters: [
+          {
+            id: "query-filter",
+            path: path.join(__dirname, "../lib/index.js"),
+            config: {
+              allow: {
+                all: ["a", "b", "c"]
+              }
+            }
+          }
+        ],
+
+        providers: [
+          {
+            id: "id",
+            context: "/provider",
+            target: "http://localhost:8888"
+          }
+        ]
+      };
+
+      // Create server with clyde's middleware options
+      var middleware = clyde.createMiddleware(options);
+      server = http.createServer(middleware);
+      server.listen(options.port);
+
+      // Make request which expects a 404 error
+      request("http://localhost:8888")
+        .get("/foo?a&b&d")
+        .expect(400, done);
+    });
+
   });
 
+  describe("deny", function() {
+
+    it("should block the request because has some of the 'any' parameters", function(done) {
+      var options = {
+        port: 8888,
+        logfile: path.join(__dirname, "..", "tmp", "clyde.log"),
+        loglevel: "info",
+
+        prefilters: [
+          {
+            id: "query-filter",
+            path: path.join(__dirname, "../lib/index.js"),
+            config: {
+              deny: {
+                any: ["a", "b", "c"]
+              }
+            }
+          }
+        ],
+
+        providers: [
+          {
+            id: "id",
+            context: "/provider",
+            target: "http://localhost:8888"
+          }
+        ]
+      };
+
+      // Create server with clyde's middleware options
+      var middleware = clyde.createMiddleware(options);
+      server = http.createServer(middleware);
+      server.listen(options.port);
+
+      // Make request which expects a 404 error
+      request("http://localhost:8888")
+        .get("/foo?c")
+        .expect(400, done);
+    });
+
+
+    it("should success because the request does not have any of the required 'any' parameters", function(done) {
+      var options = {
+        port: 8888,
+        logfile: path.join(__dirname, "..", "tmp", "clyde.log"),
+        loglevel: "info",
+
+        prefilters: [
+          {
+            id: "query-filter",
+            path: path.join(__dirname, "../lib/index.js"),
+            config: {
+              deny: {
+                any: ["a", "b"]
+              }
+            }
+          }
+        ],
+
+        providers: [
+          {
+            id: "id",
+            context: "/provider",
+            target: "http://localhost:8888"
+          }
+        ]
+      };
+
+      // Create server with clyde's middleware options
+      var middleware = clyde.createMiddleware(options);
+      server = http.createServer(middleware);
+      server.listen(options.port);
+
+      // Make request which expects a 404 error
+      request("http://localhost:8888")
+        .get("/foo?c&d")
+        .expect(404, done);
+    });
+
+    it("should block the request because the request has all the 'all' parameters", function(done) {
+      var options = {
+        port: 8888,
+        logfile: path.join(__dirname, "..", "tmp", "clyde.log"),
+        loglevel: "info",
+
+        prefilters: [
+          {
+            id: "query-filter",
+            path: path.join(__dirname, "../lib/index.js"),
+            config: {
+              deny: {
+                all: ["a", "b", "c"]
+              }
+            }
+          }
+        ],
+
+        providers: [
+          {
+            id: "id",
+            context: "/provider",
+            target: "http://localhost:8888"
+          }
+        ]
+      };
+
+      // Create server with clyde's middleware options
+      var middleware = clyde.createMiddleware(options);
+      server = http.createServer(middleware);
+      server.listen(options.port);
+
+      // Make request which expects a 404 error
+      request("http://localhost:8888")
+        .get("/foo?a&b=10&c=hi&d")
+        .expect(400, done);
+    });
+
+
+    it("should success because the request does not have all the required 'all' parameters", function(done) {
+      var options = {
+        port: 8888,
+        logfile: path.join(__dirname, "..", "tmp", "clyde.log"),
+        loglevel: "info",
+
+        prefilters: [
+          {
+            id: "query-filter",
+            path: path.join(__dirname, "../lib/index.js"),
+            config: {
+              deny: {
+                all: ["a", "b", "c"]
+              }
+            }
+          }
+        ],
+
+        providers: [
+          {
+            id: "id",
+            context: "/provider",
+            target: "http://localhost:8888"
+          }
+        ]
+      };
+
+      // Create server with clyde's middleware options
+      var middleware = clyde.createMiddleware(options);
+      server = http.createServer(middleware);
+      server.listen(options.port);
+
+      // Make request which expects a 404 error
+      request("http://localhost:8888")
+        .get("/foo?a&b&d")
+        .expect(404, done);
+    });
+
+  });
 
 });
